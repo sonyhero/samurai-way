800e8514642b922631362906360688f52cb31239
sonyhero <27anton06@gmail.com>
2023-03-03 20:37:44 +0300

    add-profile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 
 const App = () => {
@@ -9,26 +10,7 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <div className='content'>
-                <div>
-                    <img
-                        src="https://cdn.wallpapersafari.com/66/25/slYcCE.jpg"
-                        alt="itachi content" width='1200px' height='200px'/>
-                </div>
-                <div>
-                    ava+dicr
-                </div>
-                <div>
-                    my posts
-                    <div>
-                        new post
-                    </div>
-                    <div>
-                        <div>post 1</div>
-                        <div>post 2</div>
-                    </div>
-                </div>
-            </div>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://cdn.wallpapersafari.com/66/25/slYcCE.jpg"
+                    alt="itachi content" width='1200px' height='200px'/>
+            </div>
+            <div>
+                ava+dicr
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
