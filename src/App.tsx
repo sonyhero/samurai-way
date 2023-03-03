b25297e2e6f1a1bf0fe2b377f0e74158001f7fa5
sonyhero <27anton06@gmail.com>
2023-03-03 20:34:38 +0300

    add-header-and-navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
 
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLxOuoX74j_i0OvkPIIwhN6uVhK4KXc9JPjQ&usqp=CAU"
-                    alt="itachi logo"/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href="">Profile</a>
-                </div>
-                <div>
-                    <a href="">Messages</a>
-                </div>
-                <div>
-                    <a href="">News</a>
-                </div>
-                <div>
-                    <a href="">Music</a>
-                </div>
-                <div>
-                    <a href="">Settings</a>
-                </div>
-            </nav>
+            <Header/>
+            <Navbar/>
             <div className='content'>
                 <div>
                     <img
                         src="https://cdn.wallpapersafari.com/66/25/slYcCE.jpg"
                         alt="itachi content" width='1200px' height='200px'/>
-                </div>
-                <div>
-                    <img
-                        src="https://mir-s3-cdn-cf.behance.net/projects/404/a856ac92777323.Y3JvcCwxNTAwLDExNzMsMCwxNjM.png"
-                        alt="itachi" width='200px' height='200px'/>
                 </div>
                 <div>
                     ava+dicr
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLxOuoX74j_i0OvkPIIwhN6uVhK4KXc9JPjQ&usqp=CAU"
+                alt="itachi logo"/>
+        </header>
+    )
+}
