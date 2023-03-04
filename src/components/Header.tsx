a065bd1f38eda0e0682898589a0cbde064bea114
sonyhero <27anton06@gmail.com>
2023-03-04 21:40:28 +0300

    css-modules-lesson14.2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './HeaderCSS.css'
+import s from './HeaderCSS.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLxOuoX74j_i0OvkPIIwhN6uVhK4KXc9JPjQ&usqp=CAU"
                 alt="itachi logo"/>
