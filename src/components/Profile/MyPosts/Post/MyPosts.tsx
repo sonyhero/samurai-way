3f2e242c33876e59d1f7c7f85d0744e504ebb581
sonyhero <27anton06@gmail.com>
2023-03-05 00:04:29 +0300

    lesson-15-add-my-posts

---
+++ src/components/Profile/MyPosts/Post/MyPosts.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import s from './MyPostsCSS.module.css'
+
+export const MyPosts = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src="https://cdn.wallpapersafari.com/66/25/slYcCE.jpg"
+                    alt="itachi content" width='1200px' height='200px'/>
+            </div>
+            <div>
+                ava+dicr
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div className='posts'>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
