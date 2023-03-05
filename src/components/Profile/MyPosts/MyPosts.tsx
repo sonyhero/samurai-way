6776d2781d69bc1177292b84f7383deca3ba381a
sonyhero <27anton06@gmail.com>
2023-03-05 13:48:57 +0300

    lesson16-add-props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,9 +12,8 @@ export const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message='Hi, how are you?' likes={23}/>
+                <Post message="It's my first post!" likes={100}/>
             </div>
         </div>
 
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import s from './PostCSS.module.css'
 
-export const Post = () => {
+type PostType = {
+    message: string
+    likes: number
+}
+
+export const Post = (props: PostType) => {
     return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/736x/11/f7/83/11f78374741b89e4dea99e0b6356ee3c.jpg" alt="itachi logo"/>
-            post 1
+            {props.message}
+            <div>
+                <span> {props.likes} likes</span>
+            </div>
         </div>
     )
 }
