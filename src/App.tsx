65dadf41c46137c72ab534fa78674ed04ab86566
sonyhero <27anton06@gmail.com>
2023-03-11 12:38:32 +0300

    lesson-18-add-dialogs-component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from './components/Dialogs/Dialogs';
 
 
 const App = () => {
@@ -10,7 +11,8 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <Dialogs/>
+            {/*<Profile/>*/}
         </div>
     );
 }
