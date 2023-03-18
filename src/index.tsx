c0b8319e7d2849e1e2e489298f8e9d844d42f896
sonyhero <27anton06@gmail.com>
2023-03-18 18:40:48 +0300

    lesson-27-transfer-data-to-index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+// Props Dialogs-DialogsItem
+let dialogs = [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Sveta'},
+    {id: 3, name: 'Viktor'},
+    {id: 4, name: 'Maks'},
+    {id: 5, name: 'Igor'},
+]
+//Props Dialogs-Message
+let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'Anton'},
+    {id: 3, message: 'How are you'},
+]
+
+//Props Profile-MyPosts
+let posts =  [
+    {id: 1, message: 'Hi, how are you?', likesCount: 23},
+    {id: 2, message: 'It\'s my first post!', likesCount: 100}
+]
+
+let data = {
+    dialogs,
+    messages,
+    posts
+}
+
 ReactDOM.render(
-    <App />,
+    <App data={data}/>,
   document.getElementById('root')
 );
