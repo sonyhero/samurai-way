aec4b3de1ff3597d09d4a86d3212a056855a7ee4
sonyhero <27anton06@gmail.com>
2023-03-18 18:41:28 +0300

    lesson-27-transfer-use-render-in-app

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,43 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
 import {Settings} from './components/Settings/Settings';
+import {DialogItemType} from './components/Dialogs/DialogItem/DialogsItem';
+import {MessageType} from './components/Dialogs/Message/Message';
+import {PostsType} from './components/Profile/MyPosts/Post/Post';
 
+type AppPropsType = {
+    dialogs: DialogItemType[]
+    messages: MessageType[]
+    posts: PostsType[]
+}
 
-const App = () => {
+type PropsType = {
+    data: AppPropsType
+}
+
+const App = (props: PropsType) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path="/dialogs" component={Dialogs}/>
-                    <Route path="/profile" component={Profile}/>
-                    <Route path="/news" component={News}/>
-                    <Route path="/music" component={Music}/>
-                    <Route path="/settings" component={Settings}/>
+                    {/*<Route path="/dialogs" component={Dialogs}/>*/}
+                    {/*<Route path="/profile" component={Profile}/>*/}
+                    {/*<Route path="/news" component={News}/>*/}
+                    {/*<Route path="/music" component={Music}/>*/}
+                    {/*<Route path="/settings" component={Settings}/>*/}
+
+                    <Route path="/dialogs" render={()=> <Dialogs
+                        dialogs={props.data.dialogs}
+                        messages={props.data.messages}
+                    />}/>
+                    <Route path="/profile" render={()=> <Profile
+                        posts={props.data.posts}
+                    />}/>
+                    <Route path="/news" render={()=> <News/>}/>
+                    <Route path="/music" render={()=> <Music/>}/>
+                    <Route path="/settings" render={()=> <Settings/>}/>
                 </div>
             </div>
         </BrowserRouter>
