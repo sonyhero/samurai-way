b19187fb69fad7752f36cc984c71eed88d4ba134
sonyhero <27anton06@gmail.com>
2023-03-26 22:06:29 +0300

    lesson-35

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
-import {rerenderEntireTree} from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App, {StateType} from './App';
+import {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from './redux/state';
 import state from './redux/state';
 
+const rerenderEntireTree = (state: StateType) => {
+    ReactDOM.render(
+        <App
+            state={state}
+            addPost={addPost}
+            updateNewPostText={updateNewPostText}
+            addMessage={addMessage}
+            updateNewMessageText={updateNewMessageText}
+        />,
+        document.getElementById('root')
+    );
+}
+
 rerenderEntireTree(state)
+
+subscribe(()=>rerenderEntireTree(state))
