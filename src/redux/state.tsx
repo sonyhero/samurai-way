369f2b8a94a45b91c5165313a2acd09f17f23c16
sonyhero <27anton06@gmail.com>
2023-04-03 18:54:28 +0300

    lesson-37-refactoring-add-store

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,40 +1,104 @@
 import {MessageType, PostsType} from '../App';
 
-let rerenderEntireTree = () => {
-    console.log()
-}
-
-const state = {
-    profilePage: {
-        posts: [ //Props Profile-MyPosts
-            {id: 1, message: 'Hi, how are you?', likesCount: 23},
-            {id: 2, message: 'It\'s my first post!', likesCount: 100}
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        profilePage: {
+            posts: [ //Props Profile-MyPosts
+                {id: 1, message: 'Hi, how are you?', likesCount: 23},
+                {id: 2, message: 'It\'s my first post!', likesCount: 100}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [ // Props Dialogs-DialogsItem
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Sveta'},
+                {id: 3, name: 'Viktor'},
+                {id: 4, name: 'Maks'},
+                {id: 5, name: 'Igor'},
+            ],
+            messages: [ //Props Dialogs-Message
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Anton'},
+                {id: 3, message: 'How are you'},
+            ],
+            newMessageText: ''
+        },
+        sidebar: {
+            friends: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Nikita'},
+                {id: 3, name: 'Eugenia'}
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [ // Props Dialogs-DialogsItem
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Sveta'},
-            {id: 3, name: 'Viktor'},
-            {id: 4, name: 'Maks'},
-            {id: 5, name: 'Igor'},
-        ],
-        messages: [ //Props Dialogs-Message
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'Anton'},
-            {id: 3, message: 'How are you'},
-        ],
-        newMessageText: ''
+    _callSubscriber() {
+        console.log('State changed')
     },
-    sidebar: {
-        friends: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Nikita'},
-            {id: 3, name: 'Eugenia'}
-        ]
+    addPost() {
+        const newPost: PostsType = {
+            id: new Date().getTime(), message: this._state.profilePage.newPostText, likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber()
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber()
+    },
+    addMessage() {
+        const newMessage: MessageType = {
+            id: new Date().getTime(), message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber()
+    },
+    updateNewMessageText(newMessageText: string) {
+        this._state.dialogsPage.newMessageText = newMessageText
+        this._callSubscriber()
+    },
+    subscribe(observer: () => void) {
+        this._callSubscriber = observer
     }
 }
+
+// const rerenderEntireTree = () => {
+//     console.log('State changed')
+// }
+
+// const state = {
+//     profilePage: {
+//         posts: [ //Props Profile-MyPosts
+//             {id: 1, message: 'Hi, how are you?', likesCount: 23},
+//             {id: 2, message: 'It\'s my first post!', likesCount: 100}
+//         ],
+//         newPostText: ''
+//     },
+//     dialogsPage: {
+//         dialogs: [ // Props Dialogs-DialogsItem
+//             {id: 1, name: 'Dimych'},
+//             {id: 2, name: 'Sveta'},
+//             {id: 3, name: 'Viktor'},
+//             {id: 4, name: 'Maks'},
+//             {id: 5, name: 'Igor'},
+//         ],
+//         messages: [ //Props Dialogs-Message
+//             {id: 1, message: 'Hi'},
+//             {id: 2, message: 'Anton'},
+//             {id: 3, message: 'How are you'},
+//         ],
+//         newMessageText: ''
+//     },
+//     sidebar: {
+//         friends: [
+//             {id: 1, name: 'Dimych'},
+//             {id: 2, name: 'Nikita'},
+//             {id: 3, name: 'Eugenia'}
+//         ]
+//     }
+// }
 
 // export const addPost = (postMessage: string) => {
 //     const newPost: PostsType = {
@@ -45,37 +109,40 @@ const state = {
 // }
 
 // Функции добавленияя постов
-export const addPost = () => {
-    const newPost: PostsType = {
-        id: new Date().getTime(), message: state.profilePage.newPostText, likesCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree()
-}
+// export const addPost = () => {
+//     const newPost: PostsType = {
+//         id: new Date().getTime(), message: state.profilePage.newPostText, likesCount: 0
+//     }
+//     state.profilePage.posts.push(newPost)
+//     state.profilePage.newPostText = ''
+//     rerenderEntireTree()
+// }
 
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree()
-}
+// export const updateNewPostText = (newText: string) => {
+//     state.profilePage.newPostText = newText
+//     rerenderEntireTree()
+// }
 
 // Функции добавленияя сообщений
-export const addMessage = () => {
-    const newMessage: MessageType = {
-        id: new Date().getTime(), message: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messages.push(newMessage)
-    state.dialogsPage.newMessageText = ''
-    rerenderEntireTree()
-}
+// export const addMessage = () => {
+//     const newMessage: MessageType = {
+//         id: new Date().getTime(), message: state.dialogsPage.newMessageText
+//     }
+//     state.dialogsPage.messages.push(newMessage)
+//     state.dialogsPage.newMessageText = ''
+//     rerenderEntireTree()
+// }
 
-export const updateNewMessageText = (newMessageText: string) => {
-    state.dialogsPage.newMessageText = newMessageText
-    rerenderEntireTree()
-}
+// export const updateNewMessageText = (newMessageText: string) => {
+//     state.dialogsPage.newMessageText = newMessageText
+//     rerenderEntireTree()
+// }
 
-export const subscribe = (observer: ()=>void) => {
-    rerenderEntireTree = observer
-}
+// export const subscribe = (observer: () => void) => {
+//     rerenderEntireTree = observer
+// }
 
-export default state;
+// export default state
+//window.state = state
+export default store;
+// window.store = store
