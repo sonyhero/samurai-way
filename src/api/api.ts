0b90e0bb0b5e440327dd912289e4658dd18e8611
sonyhero <27anton06@gmail.com>
2023-05-28 13:18:43 +0300

    lesson-64-add-response-type

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {ProfileType} from '../redux/profile-reducer';
 
 const instance = axios.create({
     withCredentials: true,
@@ -7,6 +8,12 @@ const instance = axios.create({
         'API-KEY': '275a40de-010f-4090-ae39-5e228c881a39'
     }
 })
+
+type FollowResponse<D> = {
+    resultCode: number
+    messages: string[]
+    data: D
+}
 
 export const usersAPI = {
     getUsers(currentPage: number, pageSize: number) {
@@ -18,15 +25,15 @@ export const usersAPI = {
     },
     //----------------------------------------------------
     followUsers(userId: number) {
-        return instance.post(`follow/${userId}`)
+        return instance.post<FollowResponse<{}>>(`follow/${userId}`)
             .then(response => response.data)
     },
     unFollowUsers(userId: number) {
-        return instance.delete(`follow/${userId}`)
+        return instance.delete<FollowResponse<{}>>(`follow/${userId}`)
             .then(response => response.data)
     },
     getProfile(userId: string) {
-        return instance.get(`profile/${userId}`)
+        return instance.get<ProfileType>(`profile/${userId}`)
             .then(response => response.data)
     }
 }
