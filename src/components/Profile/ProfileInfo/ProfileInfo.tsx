c7628ad77f19209fa37e7769e2c9541f6ba97997
sonyhero <27anton06@gmail.com>
2023-06-13 17:59:15 +0300

    lesson-84-85-useState-useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import s from './ProfileInfoCSS.module.css'
-import {Preloader} from "../../common/Preloader/Preloader";
-import {ProfileType} from "../../../redux/profile-reducer";
-import {ProfileStatus} from './ProfileStatus';
+import {Preloader} from '../../common/Preloader/Preloader';
+import {ProfileType} from '../../../redux/profile-reducer';
+import {ProfileStatusUseState} from './ProfileStatusUseState';
 
 type ProfileInfoPropsType = {
     profile: ProfileType | null
@@ -31,7 +31,7 @@ export const ProfileInfo: React.FC<ProfileInfoPropsType> = (props) => {
                 }
                 ava+description
             </div>
-            Status:<ProfileStatus status={profileStatus}
+            Status:<ProfileStatusUseState status={profileStatus}
                                   updateProfileStatus={updateProfileStatus}
         />
         </div>
---
+++ src/components/Profile/ProfileInfo/ProfileStatusUseState.tsx
@@ -0,0 +1,55 @@
+import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from 'react';
+
+
+type ProfileStatusType = {
+    status: string
+    updateProfileStatus: (status: string) => void
+}
+
+export const ProfileStatusUseState: React.FC<ProfileStatusType> = (props) => {
+
+    const {status, updateProfileStatus} = props
+
+    const [editMode, setEditMode] = useState<boolean>(false)
+    const [statusProfile, setStatusProfile] = useState<string>(status)
+
+    useEffect(() => {
+        setStatusProfile(status)
+    }, [status])
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+
+    const deActivateEditMode = () => {
+        setEditMode(false)
+        updateProfileStatus(statusProfile)
+    }
+
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => setStatusProfile(e.currentTarget.value)
+
+    const onKeyHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            deActivateEditMode()
+        }
+    }
+
+    return (
+        <>
+            {editMode
+                ? <div>
+                    <input autoFocus
+                           onBlur={deActivateEditMode}
+                           value={statusProfile}
+                           onChange={onChangeStatus}
+                           onKeyDown={onKeyHandler}
+                    /></div>
+                : <div>
+                    <span onDoubleClick={activateEditMode}>
+                        {status || 'No status'}
+                    </span></div>
+
+            }
+        </>
+    )
+}
