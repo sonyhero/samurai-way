f1b5d1323a8cf718375468db8b4312c02ff11eea
sonyhero <27anton06@gmail.com>
2023-08-24 17:10:26 +0300

    feat: add scss variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppMain } from './app/App'
+import './styles/index.scss'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/700.css'
 
 ReactDOM.render(<AppMain />, document.getElementById('root'))
