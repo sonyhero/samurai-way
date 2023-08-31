066b1e7521e5bcb1b4a33a71b047173e39edbac1
sonyhero <27anton06@gmail.com>
2023-08-31 15:10:46 +0300

    fix: ProfileDescription

--- src/components/Pages/Profile/ProfileInfo/ProfileDescription/ProfileDescription.tsx
+++ src/components/Pages/Profile/ProfileInfo/ProfileDescription/ProfileDescription.tsx
@@ -4,11 +4,19 @@ import { Contacts } from './Contacts'
 import { Button } from '../../../../ui/button'
 import { Typography } from '../../../../ui/typography'
 import s from './ProfileDescription.module.scss'
+import { Modal } from '../../../../ui/modal'
 
 export const ProfileDescription: React.FC<ProfileDescriptionType> = ({ profile, isOwner, setEditMode }) => {
   return (
     <div className={s.wrap}>
       <div className={s.buttonBox}>
+        <Modal>
+          <div>Looking for a job: {profile.lookingForAJob ? 'yes' : ' no'}</div>
+          {profile.lookingForAJob && <div>My professional skills: {profile.lookingForAJobDescription}</div>}
+          <div>
+            Contacts: <Contacts />
+          </div>
+        </Modal>
         <Button>More</Button>
         {isOwner && <Button onClick={setEditMode}>Edit profile</Button>}
       </div>
