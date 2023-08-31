ab1d2a6bf3807874ca023f1d87844b11d58ee1de
sonyhero <27anton06@gmail.com>
2023-08-31 15:08:34 +0300

    feat: add modal

---
+++ src/components/ui/modal/modal.tsx
@@ -0,0 +1,71 @@
+import { ComponentProps, FC } from 'react'
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+} from '@radix-ui/react-dialog'
+
+import s from './modal.module.scss'
+import {Typography} from "../typography";
+import {Button} from "../button";
+import {Close} from "../../../assets";
+
+
+type PropsType = {
+  open?: boolean
+  onClose?: () => void
+  showCloseButton?: boolean
+  title?: string
+  titleButton?: string
+  callBack?: () => void
+} & ComponentProps<'div'>
+
+
+export const Modal: FC<PropsType> = ({
+  open = false,
+  title,
+  onClose,
+  children,
+  titleButton,
+  showCloseButton = true,
+  callBack,
+}) => {
+  function handleModalClosed() {
+    onClose?.()
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleModalClosed}>
+      {open && (
+        <DialogPortal>
+            <DialogOverlay className={s.overlay} />
+            <DialogContent className={s.content}>
+              <header className={s.header}>
+                <DialogTitle asChild>
+                  <Typography variant={'h2'}>{title}</Typography>
+                </DialogTitle>
+                {showCloseButton && (
+                  <DialogClose className={s.closeButton}>
+                    <Close />
+                  </DialogClose>
+                )}
+              </header>
+              <div className={s.contentBox}>{children}</div>
+              <div className={s.buttonBottom}>
+                <Button onClick={() => onClose?.()} variant={'secondary'}>
+                  Cancel
+                </Button>
+                <Button variant={'primary'} onClick={callBack}>
+                  {titleButton}
+                </Button>
+              </div>
+            </DialogContent>
+        </DialogPortal>
+      )}
+    </Dialog>
+  )
+}
