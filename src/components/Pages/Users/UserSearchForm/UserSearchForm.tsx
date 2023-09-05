07d34d78e55f66f739162631f07348ffe93f5d79
sonyhero <27anton06@gmail.com>
2023-09-05 15:09:46 +0300

    add formik in UserSearchForm

--- src/components/Pages/Users/UserSearchForm/UserSearchForm.tsx
+++ src/components/Pages/Users/UserSearchForm/UserSearchForm.tsx
@@ -1,27 +1,63 @@
-import * as Form from '@radix-ui/react-form'
+import s from './UserSearchForm.module.scss'
+import { FC, memo } from 'react'
+import { useSelector } from 'react-redux'
+import { getUsersFilter } from '../../../../app/selectors/users-selector'
+import { SearchFilterType } from '../user-reducer/users-reducer'
+import { Field, Formik } from 'formik'
 import { TextField } from '../../../ui/textfield'
 import { Button } from '../../../ui/button'
-import s from './UserSearchForm.module.scss'
-import { FC, FormEvent, memo } from 'react'
+
+type FriendFormType = 'true' | 'false' | 'null'
+type FormType = {
+  term: string
+  friend: FriendFormType
+}
 
 type PropsType = {
-  onFilterChange: (event: FormEvent<HTMLFormElement>) => void
+  onFilterChange: (filter: SearchFilterType) => void
 }
 
 export const UserSearchForm: FC<PropsType> = memo(({ onFilterChange }) => {
+  const filter = useSelector(getUsersFilter)
+  const submit = (values: FormType, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
+    const filter: SearchFilterType = {
+      term: values.term,
+      friend: values.friend === 'null' ? null : values.friend === 'true',
+    }
+    onFilterChange(filter)
+    setSubmitting(false)
+  }
+  const options = [
+    { value: 'null', item: 'all' },
+    { value: 'true', item: 'only followed' },
+    { value: 'false', item: 'only unfollowed' },
+  ]
+
   return (
-    <Form.Root onSubmit={onFilterChange}>
-      <div className={s.userField}>
-        <Form.Field name={'term'}>
-          <Form.Label>User</Form.Label>
-          <Form.Control asChild>
-            <TextField placeholder={'type to find user...'} />
-          </Form.Control>
-        </Form.Field>
-        <Form.Submit asChild>
-          <Button className={s.submit}>Find user</Button>
-        </Form.Submit>
-      </div>
-    </Form.Root>
+    <Formik
+      enableReinitialize
+      initialValues={{ term: filter.term, friend: String(filter.friend) as FriendFormType }}
+      onSubmit={submit}
+    >
+      {({ values, handleSubmit, isSubmitting, handleChange }) => (
+        <form onSubmit={handleSubmit} className={s.userField}>
+          <TextField
+            className={s.input}
+            name={'term'}
+            onChange={handleChange}
+            value={values.term}
+            placeholder={'type to search...'}
+          />
+          <Field as="select" name="friend" className={s.select}>
+            {options.map((el) => (
+              <option key={el.value} value={el.value} className={s.content}>
+                {el.item}
+              </option>
+            ))}
+          </Field>
+          <Button disabled={isSubmitting}>search</Button>
+        </form>
+      )}
+    </Formik>
   )
 })
