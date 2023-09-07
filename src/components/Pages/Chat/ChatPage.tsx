b9fdc9b8f6dead63049426cb55dc68cd1ec7aef6
sonyhero <27anton06@gmail.com>
2023-09-07 21:13:07 +0300

    feat: chat WebSocket part1.1

--- src/components/Pages/Chat/ChatPage.tsx
+++ src/components/Pages/Chat/ChatPage.tsx
@@ -1,4 +1,68 @@
+import { Button } from '../../ui/button'
+import { TextField } from '../../ui/textfield'
+import { FC } from 'react'
+
 const ChatPage = () => {
-  return <div>Chat</div>
+  return (
+    <div>
+      <Chat />
+    </div>
+  )
 }
+
+const Chat = () => {
+  return (
+    <div style={{ width: '500px', border: '1px solid red' }}>
+      Chat
+      <ChatMessages />
+      <AddChatMessageForm />
+    </div>
+  )
+}
+
+const ChatMessages = () => {
+  const messages: MessagesType[] = [
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+    { id: new Date(), text: 'hello' },
+  ]
+  const mappedMessages = messages.map((m) => <ChatMessage key={+m.id} text={m.text} />)
+  return <div style={{ height: '300px', overflowY: 'auto' }}>{mappedMessages}</div>
+}
+type MessagesType = {
+  id: Date
+  text: string
+}
+
+const ChatMessage: FC<ChatMessagePropsType> = () => {
+  return <div>message</div>
+}
+type ChatMessagePropsType = {
+  text: any
+}
+
+const AddChatMessageForm = () => {
+  return (
+    <div>
+      <TextField placeholder={'type message...'} />
+      <Button>Send</Button>
+    </div>
+  )
+}
+
 export default ChatPage
