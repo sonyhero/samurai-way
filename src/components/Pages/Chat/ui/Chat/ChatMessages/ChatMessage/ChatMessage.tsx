1ed5eb651ac38c46473b796b597d7a012a8de944
sonyhero <27anton06@gmail.com>
2023-09-14 23:39:48 +0300

    style: add style chat-message

--- src/components/Pages/Chat/ui/Chat/ChatMessages/ChatMessage/ChatMessage.tsx
+++ src/components/Pages/Chat/ui/Chat/ChatMessages/ChatMessage/ChatMessage.tsx
@@ -1,13 +1,25 @@
 import { FC, memo } from 'react'
 import { ChatMessageAPIType } from '../../../../api/chat-api'
+import userPhoto from '../../../../../../../assets/img/user.png'
+import s from './ChatMessage.module.scss'
+import { AvatarDemo } from '../../../../../../ui/avatar'
+import { Typography } from '../../../../../../ui/typography'
 
 export const ChatMessage: FC<ChatMessagePropsType> = memo(({ messageBody }) => {
   const { message, photo, userName } = messageBody
+
+  const avatar = photo || userPhoto
+
   return (
-    <div>
-      <img src={photo} alt={'photo'} />
-      <div>{userName}</div>
-      <div>{message}</div>
+    <div className={s.container}>
+      {/*<img className={s.avatar} src={avatar} alt={'photo'} />*/}
+      <div className={s.avatarContainer}>
+        <AvatarDemo src={avatar} />
+        <Typography className={s.fullName} variant={'h3'}>
+          {userName}
+        </Typography>
+      </div>
+      <Typography variant={'body1'}>{message}</Typography>
     </div>
   )
 })
