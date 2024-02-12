b5268662ea2f5fc85264697a8c28ee28e8e35406
sonyhero <27anton06@gmail.com>
2024-02-12 18:29:23 +0300

    feat: add routes constants

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,54 +1,83 @@
 import React from 'react'
 import s from './Navbar.module.scss'
-import { NavLink } from 'react-router-dom'
-import { ChatNav, MessagesNav, MusicNav, NewsNav, ProfileNav, SettingsNav } from '../../../assets/iconsNavbar'
-import { UsersNav } from '../../../assets/iconsNavbar'
+import { NavLink, useLocation } from 'react-router-dom'
+import { ChatNav, MessagesNav, MusicNav, NewsNav, ProfileNav, SettingsNav, UsersNav } from '../../../assets/iconsNavbar'
+import { PATH } from '../../../config/routes'
 
 export const Navbar = () => {
-  return (
-    <nav className={s.nav}>
-      <div className={s.item}>
-        <ProfileNav />
-        <NavLink to="/profile" activeClassName={s.activeLink}>
-          Profile
+  const { pathname } = useLocation()
+
+  const defaultColor = '#000'
+  const activeColor = '#407c87'
+
+  const navLinks = [
+    {
+      path: PATH.PROFILE,
+      value: (
+        <span>
+          <ProfileNav fill={pathname === PATH.PROFILE ? activeColor : defaultColor} /> Profile
+        </span>
+      ),
+    },
+    {
+      path: PATH.NEWS,
+      value: (
+        <span>
+          <NewsNav fill={pathname === PATH.NEWS ? activeColor : defaultColor} /> News
+        </span>
+      ),
+    },
+    {
+      path: PATH.DIALOGS,
+      value: (
+        <span>
+          <MessagesNav fill={pathname === PATH.DIALOGS ? activeColor : defaultColor} /> Messages
+        </span>
+      ),
+    },
+    {
+      path: PATH.USERS,
+      value: (
+        <span>
+          <UsersNav fill={pathname === PATH.USERS ? activeColor : defaultColor} /> Users
+        </span>
+      ),
+    },
+    {
+      path: PATH.CHAT,
+      value: (
+        <span>
+          <ChatNav fill={pathname === PATH.CHAT ? activeColor : defaultColor} /> Chat
+        </span>
+      ),
+    },
+    {
+      path: PATH.MUSIC,
+      value: (
+        <span>
+          <MusicNav fill={pathname === PATH.MUSIC ? activeColor : defaultColor} /> Music
+        </span>
+      ),
+    },
+    {
+      path: PATH.SETTINGS,
+      value: (
+        <span>
+          <SettingsNav fill={pathname === PATH.SETTINGS ? activeColor : defaultColor} /> Settings
+        </span>
+      ),
+    },
+  ]
+
+  const mappedLinks = navLinks.map((navLink, index) => {
+    return (
+      <div key={index} className={s.item}>
+        <NavLink to={navLink.path} activeClassName={s.activeLink}>
+          {navLink.value}
         </NavLink>
       </div>
-      <div className={s.item}>
-        <NewsNav />
-        <NavLink to="/news" activeClassName={s.activeLink}>
-          News
-        </NavLink>
-      </div>
-      <div className={s.item}>
-        <MessagesNav />
-        <NavLink to="/dialogs" activeClassName={s.activeLink}>
-          Messages
-        </NavLink>
-      </div>
-      <div className={s.item}>
-        <UsersNav />
-        <NavLink to="/users" activeClassName={s.activeLink}>
-          Users
-        </NavLink>
-      </div>
-      <div className={s.item}>
-        <ChatNav />
-        <NavLink to="/chat" activeClassName={s.activeLink}>
-          Chat
-        </NavLink>
-      </div>
-      <div className={s.item}>
-        <MusicNav />
-        <NavLink to="/music" activeClassName={s.activeLink}>
-          Music
-        </NavLink>
-      </div>
-      <div className={s.item}>
-        <SettingsNav />
-        <NavLink to="/settings" activeClassName={s.activeLink}>
-          Settings
-        </NavLink>
-      </div>
-    </nav>
-  )
+    )
+  })
+
+  return <nav className={s.nav}>{mappedLinks}</nav>
 }
---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+export const PATH = {
+  HOME: '/',
+  PROFILE: '/profile',
+  USERS: '/users',
+  CHAT: '/chat',
+  LOGIN: '/login',
+  NEWS: '/news',
+  MUSIC: '/music',
+  SETTINGS: '/settings',
+  DIALOGS: '/dialogs',
+}
